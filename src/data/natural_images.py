508e03080d6bec48c6578dc8608085a7c17e3eaa
Nazarii Yukhnovskyi <104125216+madbulgarianfromukraine@users.noreply.github.com>
2026-06-17 10:43:59 +0200

    labls should be 1d tensor at most

--- src/data/natural_images.py
+++ src/data/natural_images.py
@@ -141,9 +141,7 @@ class StochasticCutMixDataLoader:
         for images, labels in self.dataloader:
             if random.random() < self.p:
                 images, labels = self.cutmix(images, labels)
-            else:
-                # Convert standard integer labels to soft one-hot distributions
-                labels = F.one_hot(labels, num_classes=self.num_classes).float()
+                labels = labels.argmax(dim=1)
                 
             yield images, labels
 
